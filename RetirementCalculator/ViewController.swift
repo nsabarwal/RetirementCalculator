4a5eae2dd8c4d923b715fe6b873d44c96ce3678a
Naman Sabarwal <namans@Namans-MacBook-Pro.local>
2023-10-01 23:15:33 +0530

    track events on AppCenter

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var retirementAgeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        Analytics.trackEvent("navigated_to_calculator")
+        if Crashes.hasCrashedInLastSession {
+            //show alert to apologize when the user opens the app after the last crash of the app
+        }
+    }
 
-
+    @IBAction func actionCalculate(_ sender: UIButton) {
+        // Crashes.generateTestCrash()
+        
+        let currentAge = Int(ageTextField.text ?? "") ?? 0
+        let retirementAge = Int(retirementAgeTextField.text ?? "") ?? 0
+        
+        let properties = ["current_age": String(currentAge),
+                          "retirement_age": String(retirementAge)]
+        Analytics.trackEvent("calculate_retirement_calculated", withProperties: properties)
+    }
 }
 
